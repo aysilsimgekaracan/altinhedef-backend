3dba8fd6fca5cc0e24bcda4a008fa49d49959b68
Ayşıl Simge Karacan <github@aysilsimge.dev>
2025-08-03 00:31:50 +0300

    create kotlin spring boot project

---
+++ altinhedef/src/main/kotlin/net/altinhedef/altinhedef/AltinhedefApplication.kt
@@ -0,0 +1,11 @@
+package net.altinhedef.altinhedef
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AltinhedefApplication
+
+fun main(args: Array<String>) {
+	runApplication<AltinhedefApplication>(*args)
+}
